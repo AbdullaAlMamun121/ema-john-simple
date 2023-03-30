997f2a015ddd2ca8cced6c6397612204b8cb64bf
Abdulla <hmdabdulla121@gmail.com>
2023-03-30 13:08:37 +0600

    added advance option for adding product in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,10 +6,17 @@ const Cart = ({ cart }) => {
     // const cart = props.cart; //option-1
     let totalPrice = 0;
     let totalShipping = 0;
-    
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        // (Advance code in Shop.jsx , handleAddToCart section)
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -17,7 +24,7 @@ const Cart = ({ cart }) => {
     return (
         <div className="cart">
             <h4>Order summary</h4>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price:${ totalPrice}</p>
             <p>Shipping:${ totalShipping}</p>
             <p>Tax:${ tax.toFixed(2)}</p>
